d63af3d8ab9845cc741d7328c471841bd371454c
gastonlopez5 <43733902+gastonlopez5@users.noreply.github.com>
2021-05-31 20:19:14 -0300

    Modelos completo

---
+++ app/src/main/java/com/example/clublaribera_app/modelos/Pago.java
@@ -0,0 +1,45 @@
+package com.example.clublaribera_app.modelos;
+
+import java.io.Serializable;
+
+public class Pago implements Serializable {
+    private int id;
+    private int usuarioId;
+    private int pedidoId;
+    private String comprobante;
+
+    public Pago() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public int getPedidoId() {
+        return pedidoId;
+    }
+
+    public void setPedidoId(int pedidoId) {
+        this.pedidoId = pedidoId;
+    }
+
+    public String getComprobante() {
+        return comprobante;
+    }
+
+    public void setComprobante(String comprobante) {
+        this.comprobante = comprobante;
+    }
+}
